c49911255a05c919c386a08ff86aa1de65531d85
aheld <aarondheld@gmail.com>
2012-08-20 12:25:43 -0400

    fixed e2e test'

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,11 +4,12 @@
 
 describe('Issues App', function() {
 
-  it('should redirect index.html to index.html#/issues/new', function() {
-    browser().navigateTo('../../app/index.html');
-    expect(browser().location().url()).toBe('/issues');
+  describe('Routing', function() {
+    it('should redirect index.html to index.html#/issues/list/', function() {
+      browser().navigateTo('../../app/index.html');
+      expect(browser().location().url()).toBe('/issues/list/');
+    });
   });
-
 
   describe('Issues list view', function() {
 
@@ -18,32 +19,22 @@ describe('Issues App', function() {
 
 
     it('should filter the issues list as user types into the search box', function() {
-      expect(repeater('.issues li.issue').count()).toBe(7);
+      expect(repeater('.issues li.issue').count()).toBe(9);
 
-      input('query').enter('https');
+      input('filterQuery').enter('grooming');
       expect(repeater('.issues li.issue').count()).toBe(1);
 
-      input('query').enter('It works great');
+      input('filterQuery').enter('It works great');
       expect(repeater('.issues li.issue').count()).toBe(0);
     });
 
 
-    it('should be possible to control order via the drop down select box', function() {
-      input('query').enter(''); 
-
-      expect(repeater('.issues li.issue').column('issue.key')).
-          toEqual(["INBOUND-20","EPRT-119","EPPRO-317","EPPRO-196","EPPRO-132","EPINF-27","CISCO-135"]);
-
-      //select('orderProp').option('Priority');
-      //later on this one
-
-    });
-
+  
 
     it('should render issue previews', function() {
-      input('query').enter('https');
+      input('filterQuery').enter('grooming');
       element('.issues li.issue a.preview').click();
-      expect(browser().location().url()).toBe('/issues/INBOUND-20');
+      expect(browser().location().url()).toBe('/issues/PRICE-197');
     });
   });
 
